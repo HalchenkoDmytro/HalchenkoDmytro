cd345615933f746dbf9a4c6c837506bd4bee0f45
Dmytro Halchenko <galchenkodima1984@gmail.com>
2022-05-13 19:53:29 +0300

    Add HW27

---
+++ lesson27/min/js/myscript.js
@@ -0,0 +1,9 @@
+const yourAge = Number(prompt('Enter your age'));
+
+if (yourAge > 20 && yourAge <25) {
+  console.log('Yng');
+} else if (yourAge >= 25 && yourAge <30) {
+  console.log('man');
+} else {
+  console.log('Not Age');
+}

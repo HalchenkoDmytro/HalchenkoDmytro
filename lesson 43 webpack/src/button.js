f20af3067d427c378852e02c1a61e6058b4e519c
Dmytro Halchenko <galchenkodima1984@gmail.com>
2022-07-03 18:54:05 +0300

    add gulp

---
+++ lesson 43 webpack/src/button.js
@@ -0,0 +1,10 @@
+import jQuery from "jquery";
+
+const button = $('<button>Submit</button>');
+button.addClass('btn');
+
+//export default button;
+
+const buttonSmall = $('<button>Submit</button>');
+
+export { button, buttonSmall };

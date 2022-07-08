d506e91eba196e96f2e7b0eba218af73b2eb7197
Dmytro Halchenko <galchenkodima1984@gmail.com>
2022-07-08 21:34:49 +0300

    Add React-Component

---
+++ react-lessons/react-practice/src/component/Aside.jsx
@@ -0,0 +1,13 @@
+import Text from "./Text";
+
+const Aside = (props) => {
+
+  return (
+    <section className="aside">
+      <Text>aside</Text>
+    </section>
+  );
+ 
+}
+
+export default Aside;
--- react-lessons/react-practice/src/index.js
+++ react-lessons/react-practice/src/index.js
@@ -19,9 +19,9 @@ reportWebVitals(); */
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import header from './component/header';
-import main from './component/main';
-import footer from './component/footer';
+import Header from './component/Header';
+import Main from './component/Main';
+import Footer from './component/Footer';
 
 //const footer = React.createElement('footer', )
 //const main = React.createElement('main', null, header, footer);
@@ -29,9 +29,9 @@ import footer from './component/footer';
 const container = (
   <div className='container'>
     <h2 className='main__title'>root component</h2>
-    {header}
-    {main}
-    {footer}
+    <Header></Header>
+    <Main />
+    <Footer />
   </div>
 );
 
